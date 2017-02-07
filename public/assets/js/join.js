1c1a15a2fda4bd488190d091511a958366ebc2b5
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-02-07 10:18:13 -0600

    adding theme

--- public/assets/js/join.js
+++ public/assets/js/join.js
@@ -3,3 +3,9 @@ function showImage() {
 		$(this).append("<span class='member'><img src='../img/person.svg'></span>");
 	});
 }
+
+function getGroupLimit() {
+
+}
+var groupLimit = $('join');
+// for (var x <= $(groupLimit);
