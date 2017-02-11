f260a23770971c89b3a758096798dbefadf0373b
Josh Kim <kangsan@dhcp-10-102-154-63.wireless.northwestern.private>
2017-02-11 12:00:26 -0600

    changed callback

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:8080/auth/github/callback"
+    callbackURL: "https://damp-plains-99947.herokuapp.com/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
 
