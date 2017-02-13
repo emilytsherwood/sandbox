c551ed6cb5ce6e0a6b2d8b6eec719f51e12ef588
Josh Kim <kangsan@Joshs-MacBook-Air.local>
2017-02-12 23:41:58 -0600

    modal mismatch

--- controllers/sandbox_controller.js
+++ controllers/sandbox_controller.js
@@ -229,7 +229,7 @@ module.exports = function (app) {
                                         db.Post.findAll({}),
                                     ]).then(function (result) {
                                         var posts = result[0];
-                                        res.render("joinModal", {
+                                        res.render("joinAndEmailModal", {
                                             posts: posts,
                                             user: req.user
                                         });
@@ -245,7 +245,7 @@ module.exports = function (app) {
                                             db.Post.findAll({}),
                                         ]).then(function (result) {
                                             var posts = result[0];
-                                            res.render("joinAndEmailModal", {
+                                            res.render("joinModal", {
                                                 posts: posts,
                                                 user: req.user
                                             });
