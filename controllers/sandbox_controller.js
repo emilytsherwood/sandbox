a176afa458f359593dcb03d455e89ccdfef71269
Josh Kim <kangsan@Joshs-MacBook-Air.local>
2017-02-12 22:05:47 -0600

    fixed all modal errors, best working version

--- controllers/sandbox_controller.js
+++ controllers/sandbox_controller.js
@@ -15,7 +15,7 @@ let transporter = nodemailer.createTransport({
 // Export these awesome routes
 module.exports = function (app) {
 
-     app.get('/past', function(req, res){
+    app.get('/past', function(req, res){
         db.Post.findAll({})
         .then(function (result) {
                 
@@ -23,15 +23,15 @@ module.exports = function (app) {
             });
     });
 
+
     app.get('/terms', function (req, res) {
-    res.sendFile('terms');
+        res.sendFile('terms');
     });
     
+
     app.get('/privacy', function(req, res){
-      res.sendFile('privacy');
+        res.sendFile('privacy');
     });
-
-
 
     // Get the root route
     app.get("/", function (req, res, next) {
@@ -53,54 +53,7 @@ module.exports = function (app) {
             console.log(e);
         });
     });
-    app.get('/api/posts/:id?', function (req, res) {
-        if (req.params.id) {
-            db.Post.findOne({
-                where: {
-                    id: req.params.id
-                }
-            }).then(function (result) {
-                return res.json(result);
-            });
-        } else {
-            // Otherwise display the data for all of the characters.
-            db.Post.findAll({}).then(function (result) {
-                return res.json(result);
-            });
-        }
-    });
-    app.get('/api/users/:id?', function (req, res) {
-        if (req.params.id) {
-            db.User.findOne({
-                where: {
-                    id: req.params.id
-                }
-            }).then(function (result) {
-                return res.json(result);
-            });
-        } else {
-            // Otherwise display the data for all of the characters.
-            db.User.findAll({}).then(function (result) {
-                return res.json(result);
-            });
-        }
-    });
-    app.get('/api/groups/:id?', function (req, res) {
-        if (req.params.id) {
-            db.UserPost.findOne({
-                where: {
-                    id: req.params.id
-                }
-            }).then(function (result) {
-                return res.json(result);
-            });
-        } else {
-            // Otherwise display the data for all of the characters.
-            db.UserPost.findAll({}).then(function (result) {
-                return res.json(result);
-            });
-        }
-    });
+
     // Post for creating Ideas
     app.post('/add/', function (req, res) {
 
@@ -162,15 +115,7 @@ module.exports = function (app) {
         }
     });
 
-    app.post('/user', function (req, res) {
-        var newUser = req.body;
-        // Makes sure something is inputed
-        db.User.create({
-            user_name: newUser.fooBar
-        }).then(function (result) {
-            res.redirect('/');
-        });
-    });
+
     app.post('/post/join', function (req, res) {
         //if user is not logged in, serve modal
         if(loginBool.loggedIn===false){
@@ -194,127 +139,66 @@ module.exports = function (app) {
             var selectPostId = req.body.postId;
             var selectGroupLimit = req.body.groupLimit;
 
-                 // console.log("selectPost: " + selectPostId);
-                    Promise.all([
-                        db.Post.find({
-                            where: {
-                                id: selectPostId
-                            }
-                        }),
-                        db.User.find({
-                            where: {
-                                email: currentUser
-                            }
-                        })
-                    ]).then(function (result) {
-                        // console.log("RESULT: " + JSON.stringify(result[1]['id']));
-                        db.UserPost.findOrCreate({
-                            where: {
-                                userEmail: currentUser,
-                                UserId: result[1]['id'],
-                                PostId: selectPostId
-                        }, defaults: {
-                                userEmail: currentUser,
-                                UserId: result[1]['id'],
-                                PostId: selectPostId
-                        }})
-                        .then(function (result) {
-                            
-                            db.UserPost.findAll({
+                db.UserPost.findAll({
+                    where: {
+                        postId: selectPostId
+                    }
+                }).then(function (result) {
+                    
+                    var userAlreadyJoinBool=false;
+
+                    for (var i = 0; i < result.length; i++) {
+                        if(result[i]['userEmail'] === currentUser){
+                        userAlreadyJoinBool=true; 
+                        }
+                    }
+
+                    if(userAlreadyJoinBool){
+
+                        Promise.all([
+                            db.Post.findAll({}),
+                        ]).then(function (result) {
+                            var posts = result[0];
+                            res.render("cantJoinModal", {
+                                posts: posts,
+                                user: req.user
+                            });
+                        }).catch(function (e) {
+                            console.log(e);
+                        });
+                                      
+
+                    } else{
+                        Promise.all([
+                            db.User.find({
                                 where: {
-                                    postId: selectPostId
+                                    email: currentUser
                                 }
+                            })
+                        ]).then(function (result) {
+
+                            db.UserPost.create({
+                                userEmail: currentUser,
+                                UserId: result[0]['id'],
+                                PostId: selectPostId                
                             }).then(function (result) {
-
-                                console.log("MODALMANANANA" + JSON.stringify(result));
-
-                               if (result.length == selectGroupLimit) {
-
-                                var listOfEmails="";
-
-                                for (var i = 0; i < result.length; i++) {
-                                    var recipient = result[i]['userEmail'] + ', ';
-                                    listOfEmails = listOfEmails.concat(recipient);
-                                }
-
-                                listOfEmails = listOfEmails.slice(0, (listOfEmails.length - 2))
-
-                                    // setup email data with unicode symbols
-                                    let mailOptions = {
-                                        from: '"SandBox Team 👻" <sandboxteam321@gmail.com>', // sender address
-                                        to: listOfEmails, // list of receivers
-                                        subject: 'SANDBOX COLLOBORATION!', // Subject line
-                                        text: 'Hi! Let\'s work together!', // plain text body
-                                        html: '<b>Hi! Let\'s work together!</b>' // html body
-                                    };
-
-                                    transporter.sendMail(mailOptions, (error, info) => {
-                                        if (error) {
-                                            return console.log(error);
-                                        }
-                                        console.log('Message %s sent: %s', info.messageId, info.response);
+                                Promise.all([
+                                    db.Post.findAll({}),
+                                ]).then(function (result) {
+                                    var posts = result[0];
+                                    res.render("JoinModal", {
+                                        posts: posts,
+                                        user: req.user
                                     });
-
-                                    db.Post.update({
-                                            capacity: true
-                                        }, {                                     
-                                            where: {
-                                            id: selectPostId
-                                            }
-                                        });
-
-                                    res.redirect('/');    
-
-                               }
-
-                               else{
-
-                                    var userAlreadyJoinBool=false;
-
-                                    for (var i = 0; i < result.length; i++) {
-                                        if(result[i]['userEmail'] == currentUser){
-                                           userAlreadyJoinBool=true; 
-                                        }
-                                    }
-
-                                    if(userAlreadyJoinBool === true){
-
-                                        Promise.all([
-                                            db.Post.findAll({}),
-                                        ]).then(function (result) {
-                                            var posts = result[0];
-                                            res.render("cantJoinModal", {
-                                                posts: posts,
-                                                user: req.user
-                                            });
-                                        }).catch(function (e) {
-                                            console.log(e);
-                                        });
-
-                                    } else{
-
-                                        Promise.all([
-                                            db.Post.findAll({}),
-                                        ]).then(function (result) {
-                                            var posts = result[0];
-                                            res.render("JoinModal", {
-                                                posts: posts,
-                                                user: req.user
-                                            });
-                                        }).catch(function (e) {
-                                            console.log(e);
-                                        });
-
-                                    }
-                               }
-
+                                }).catch(function (e) {
+                                    console.log(e);
+                                });
                             });
 
-
-                        }).catch(function (err) {
-                            console.log(err);
                         });
-                    });
+                    }
+
+                });
         }
 
     });
