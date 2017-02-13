9ea6d8cd136ed13910ed76c12710c19768e0ac57
Josh Kim <kangsan@Joshs-MacBook-Air.local>
2017-02-12 23:21:10 -0600

    typo

--- controllers/pages_controller.js
+++ controllers/pages_controller.js
@@ -15,7 +15,7 @@ module.exports = function(app){
 
     app.get('/terms', function (req, res) {
         // res.sendFile('./views/terms');
-        res.sendFile(path.join(__dirname + "/./views/terms.html"));
+        res.sendFile(path.join(__dirname + "/views/terms.html"));
     });
     
 
