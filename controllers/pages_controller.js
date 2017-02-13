855a7aa33402d90567b5628dc97b61feffe0501b
maxvanbel <maxvanbel@gmail.com>
2017-02-13 00:11:09 -0600

    added routes on privacy and terms

--- controllers/pages_controller.js
+++ controllers/pages_controller.js
@@ -1,23 +1,32 @@
  // Require functions from the models folder (index.js and burger.js)
-var db = require("../models");
-var passport = require('passport');
-// page routes   
-module.exports = function(app){
-    app.get('/past', function(req, res){
-        db.Post.findAll({})
-        .then(function (result) {
-                
-            res.render('pastProjects', {posts: result});
-            });
-    });
-
-
-    app.get('/terms', function (req, res) {
-        res.render('terms');
-    });
-    
-
-    app.get('/privacy', function(req, res){
-        res.render('privacy');
-    });
-};
+ var db = require("../models");
+ var passport = require('passport');
+ // page routes
+ module.exports = function (app) {
+     app.get('/past', function (req, res) {
+         db.Post.findAll({}).then(function (result) {
+             res.render('pastProjects', {
+                 posts: result
+             });
+         });
+     });
+     app.get('/terms', function (req, res) {
+         db.User.findAll({}).then(function (result) {
+             var users = result[0];
+             res.render("index", {
+                 users: users,
+                 user: req.user
+             });
+             // res.render('/terms');
+         });
+     });
+    app.get('/privacy', function (req, res) {
+             db.User.findAll({}).then(function (result) {
+                 var users = result[0];
+                 res.render("index", {
+                     users: users,
+                     user: req.user
+                 });
+             });
+         });
+ };
