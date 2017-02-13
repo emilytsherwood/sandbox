c2b22966dc4ac00d91e6f4deb0983bec5adfa509
maxvanbel <maxvanbel@gmail.com>
2017-02-13 14:31:55 -0600

    broke the past proj page

--- controllers/pages_controller.js
+++ controllers/pages_controller.js
@@ -5,14 +5,8 @@
  module.exports = function (app) {
      app.get('/past', function (req, res) {
          db.Post.findAll({}).then(function (result) {
-                var timeStamp = result.createdAt;
-                var cutTimeStamp = timeStamp.Substring(0,16);
-                var createdAt = posts.createdAt;
-                console.log(cutTimeStamp);
              res.render('pastProjects', {
-                posts: result,
-                createdAt: cutTimeStamp
-
+                posts: result
              });
          });
      });
